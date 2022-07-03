59ef4ea3f85b640adb503ed1bac18a4470da917b
AdemolaOlaoti <105083847+AdemolaOlaoti@users.noreply.github.com>
2022-07-03 08:40:29 -0400

    Create joint.groovy

---
+++ vars/joint.groovy
@@ -0,0 +1,20 @@
+def call(String stageName) {
+    if ("${stageName}" == 'Build') {
+        sh 'mvn clean'
+        sh 'mvn install'
+    }
+    else if ("${stageName}" == 'CodeQualityAnalysis') {
+        sh 'mvn clean'
+        sh 'mvn sonar:sonar'
+    }
+    else if ("${stageName}" == 'BackupArtifacts') {
+        sh 'mvn clean'
+        sh 'mvn deploy'
+    }
+    else if ("${stageName}" == 'Authorisation') {
+         timeout(time: 48, unit: 'HOURS') {
+                    // some block
+                    input message: 'Approve or Decline'
+                    }
+    }
+}
